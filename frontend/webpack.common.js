00ba72b2eeea50618ec609fb268184c63b0e6511
Steffen Vogel <post@steffenvogel.de>
2022-06-13 00:12:14 +0200

    fix common webpack config

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    mode: "development",
     entry: "./src/index.ts",
     output: {
         filename: "main.js",
